51e49d8e58f5fc0400fe0a7c6cb91927b908a92b
Godwin <godwincyril6@gmail.com>
2025-03-19 11:54:03 +0530

    Initial Commit

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://blog-post-z8g6.onrender.com',
+  // baseURL: 'https://blog-post-z8g6.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
